cdb04554c3202068be71e32a0fd0d9438608367e
Chris Waigl <chris.waigl@gmail.com>
2017-08-03 19:37:41 -0800

    Add command line argument, make catalog generation more flexible.

--- cw-research/ginaviirscatalog.py
+++ cw-research/ginaviirscatalog.py
@@ -7,14 +7,43 @@ Chris Waigl, 2016-06-01
 
 import os
 import sys
+import glob
 import json
+import argparse
 sys.path.append("/Users/chris/Dropbox/Research/satelliteremotesensing/firedetection")
 import viirstools as vt
 
-basedir = u'/Volumes/cwdata1/VIIRS/GINA/dds.gina.alaska.edu/NPP/viirs/'
+#basedir = u'/Volumes/cwdata1/VIIRS/GINA/dds.gina.alaska.edu/NPP/viirs/'
+basedir = u'/Volumes/cwdata1/VIIRS/GINA/mirror_script'
 outdir = basedir 
 outname = u'viirsgranulecatalog.json'
 
-cata = vt.getgranulecatalog(basedir)
-with open(os.path.join(outdir, outname), 'w') as dest:
-    dest.write(json.dumps(cata, indent=2))
+def parse_arguments():
+    """Parse arguments"""
+    parser = argparse.ArgumentParser(description='generate scene catalog')
+    parser.add_argument('-o', dest='outdir', 
+        help='set outdir',
+        default=outdir)
+    parser.add_argument('-d', dest='datadir',
+        help='set data dir',
+        default=basedir)
+    parser.add_argument('--patt', dest='globpattern',
+        help='pattern to select files',)
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    outpath = os.path.join(args.outdir, outname)
+    data = {}
+    if os.path.exists(outpath):
+        with open('outpath') as data_file:    
+            data = json.load(data_file)
+    scenelist = None
+    os.chdir(args.datadir)
+    if args.globpattern is not None:
+        scenelist = glob.glob(args.globpattern)
+        scenelist = filter(os.path.isdir, scenelist)
+    cata = vt.getgranulecatalog(args.datadir, scenelist=scenelist)
+    data.update(cata)
+    with open(outpath, 'w') as dest:
+        dest.write(json.dumps(data, indent=2))
